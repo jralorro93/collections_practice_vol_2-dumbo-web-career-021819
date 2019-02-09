c5b11ebc7f73785bb02aa4cc9c8346f3b67e39ef
Juan Alorro <jralorro93@gmail.com>
2019-02-09 17:46:58 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
-# your code goes here
+require 'pry'
+
+def begins_with_r(array)
+  array.all? do |name|
+    name.start_with?("r")
+  end
+end
+
+def contain_a(array)
+  array.select do |name|
+    name.include?("a")
+  end 
+end 
+
+def first_wa(array)
+  
+  array.find do |name| 
+    binding pry
+    name.include?("wa")
+  end 
+end 
